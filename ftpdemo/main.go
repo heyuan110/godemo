88deb194efa3197a4587076064a12ed8e13322bb
Bruce He <bruce.he@patpat.com>
2019-12-20 15:17:48 +0800

    update

--- ftpdemo/main.go
+++ ftpdemo/main.go
@@ -8,26 +8,6 @@ import (
 )
 
 func main() {
-	//done := make(chan os.Signal, 1)
-	//signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-
-	//fmt.Println("hello")
-	//js,err := simplejson.NewJson([]byte(`{"a":"12","b":12}`))
-	//if err != nil {
-	//	panic("json format error")
-	//}
-	//fmt.Println(js)
-	//s1,_ := js.Get("a").String()
-	//fmt.Println("s1->",s1)
-	//s2,_ := js.Get("b").Int()
-	//fmt.Println("s2->",s2)
-	//
-	//m:= map[string]interface{}{
-	//	"key":"hello world!",
-	//}
-	//json := formatjson.Encode(m)
-	//fmt.Println(json)
-
 	n := screenshot.NumActiveDisplays()
 	for i := 0; i < n; i++ {
 		bounds := screenshot.GetDisplayBounds(i)
@@ -41,6 +21,4 @@ func main() {
 		png.Encode(file, img)
 		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
 	}
-
-	//<-done
 }
